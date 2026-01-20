b3d5c22e3f78e9b6febdea5f5e991e32c490ec79
ilyab <i-baykov@internet.ru>
2026-01-20 13:28:35 +0300

    Реализация генерации вопросов через кнопку админки

--- web/apps/interviews/admin.py
+++ web/apps/interviews/admin.py
@@ -1,10 +1,15 @@
+from admin_extra_buttons.api import ExtraButtonsMixin,button
+# from admin_extra_buttons.mixins import ExtraButtonsMixin
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from .models import (
     Candidate,
     Vacancy,
     Interview,
     InterviewQuestion, InterviewMessage,
 )
+from .services.ai_question_generation.generator import ai_question_generator
+from .services.interview_preparation import InterviewPreparationService
 
 
 @admin.register(Candidate)
@@ -51,7 +56,8 @@ class InterviewMessageInline(admin.TabularInline):
 
 
 @admin.register(Interview)
-class InterviewAdmin(admin.ModelAdmin):
+class InterviewAdmin(ExtraButtonsMixin, admin.ModelAdmin):
+
     """
     Админка для интервью.
     Позволяет создавать интервью, добавлять вопросы.
@@ -63,3 +69,27 @@ class InterviewAdmin(admin.ModelAdmin):
     readonly_fields = ("token_link", "started_at", "completed_at", "total_score")
     inlines = [InterviewQuestionInline, InterviewMessageInline]
     ordering = ("-started_at",)
+
+    @button(
+        label="Сгенерировать вопросы",
+        html_attrs={"style": "background-color:#e67e22; color:white; font-weight:bold;"},
+        change_form=True,  # показывать только на странице редактирования
+        change_list=False,  # не показывать на странице списка
+    )
+    def generate_questions(self, request, pk):
+        """
+        Генерация вопросов для интервью через Celery-задачу
+        """
+        try:
+            interview = Interview.objects.select_related("candidate", "vacancy").get(id=pk)
+
+            service = InterviewPreparationService(question_generator=ai_question_generator)
+            service.prepare_interview(interview=interview, questions_count=5)
+
+            self.message_user(request, "Задача на генерацию вопросов поставлена в очередь.", level="success")
+        except Interview.DoesNotExist:
+            self.message_user(request, "Интервью не найдено.", level="error")
+        except Exception as e:
+            self.message_user(request, f"Ошибка: {str(e)}", level="error")
+
+        return HttpResponseRedirect(request.get_full_path())
