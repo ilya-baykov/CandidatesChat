1582fd41bd3a8c48873afba0e4f55fe0704c324c
ilyab <i-baykov@internet.ru>
2026-02-05 12:19:44 +0300

    Реализован подсчёт total_score

--- web/apps/interviews/services/interviews.py
+++ web/apps/interviews/services/interviews.py
@@ -1,7 +1,8 @@
 from django.utils import timezone
+from django.db import models
 
 from ..collections import InterviewCodes
-from ..models import Interview
+from ..models import Interview, InterviewAnswer
 from ..services.questions import QuestionService
 from ...reference.models import InterviewStatus
 
@@ -13,9 +14,10 @@ class InterviewService:
     def complete_if_done(interview: Interview) -> bool:
         """Завершает интервью, если все вопросы answered."""
         if not QuestionService.get_current(interview):
+            interview.total_score = InterviewService._calculate_total_score(interview)
             interview.status = InterviewStatus.objects.get(code=InterviewCodes.COMPLETED)
             interview.completed_at = timezone.now()
-            interview.save(update_fields=["status", "completed_at"])
+            interview.save(update_fields=["status", "completed_at", "total_score"])
             return True
         return False
 
@@ -43,3 +45,10 @@ class InterviewService:
         status_obj = InterviewStatus.objects.get(code=status_code)
         interview.status = status_obj
         interview.save(update_fields=["status"])
+
+    @staticmethod
+    def _calculate_total_score(interview: Interview) -> float:
+        """Вычисляет средний score всех ответов интервью."""
+        answers = InterviewAnswer.objects.filter(interview_question__interview=interview, score__isnull=False)
+        avg_score = answers.aggregate(avg_score=models.Avg('score'))['avg_score']
+        return avg_score or 0.0
